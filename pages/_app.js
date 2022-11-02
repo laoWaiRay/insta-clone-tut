ce1db6fb64a04fafe819712106ac1f696342d979
Ray <raymondly96@gmail.com>
2022-11-02 01:45:48 -0700

    Add meta tag for mobile

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
 import { RecoilRoot } from 'recoil';
-
+import Head from 'next/head';
 function MyApp({
   Component, 
   pageProps: { session, ...pageProps }
@@ -9,6 +9,9 @@ function MyApp({
   return (
     <SessionProvider session={session}>
       <RecoilRoot>
+        <Head>
+          <meta name="viewport" content="width=device-width" />
+        </Head>
         <Component {...pageProps} />
       </RecoilRoot>
     </SessionProvider>
